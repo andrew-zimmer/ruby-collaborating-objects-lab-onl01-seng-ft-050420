816e5065180ec97b6ab95bdf1f6b979f5929b8c4
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-22 14:11:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,11 @@
 class Song 
   @@all = []
   attr_accessor :name, :artist 
+<<<<<<< HEAD
   def initialize(name)
+=======
+  def initialize
+>>>>>>> f49b6309d77d9f3400b3fb30fd3ee12c88db272f
     @name = name 
     @@all << self 
   end 
@@ -11,6 +15,7 @@ class Song
   
   def self.new_by_filename(mp3)
     array = mp3.split(" - ")
+<<<<<<< HEAD
     song = Song.new(array[1])
     Artist.find_or_create_by_name(array[0]).add_song(song)
     song
@@ -18,5 +23,14 @@ class Song
   
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
+=======
+    Song.new(array[1]).artist = array[0]
+    
+    
+  end 
+  
+  def artist_name=(name)
+    Artist.find_or_create_by_name(name)
+>>>>>>> f49b6309d77d9f3400b3fb30fd3ee12c88db272f
   end 
 end 
